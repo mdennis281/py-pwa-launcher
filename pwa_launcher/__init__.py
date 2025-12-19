6705470e274f92b6d5a1a16b491db952b4920e2f
Michael Dennis <mdennis281@gmail.com>
2025-12-19 08:14:34 -0600

    fix import issue

--- pwa_launcher/__init__.py
+++ pwa_launcher/__init__.py
@@ -1,7 +1,11 @@
 
-from pwa_launcher.get_chromium import get_chromium_install, get_chromium_installs
+from pwa_launcher.get_chromium import ( 
+    get_chromium_install, 
+    get_chromium_installs,
+    ChromiumNotFoundError
+)
 from pwa_launcher.pwa_support import check_pwa_support, PWACheckResult
-from pwa_launcher.open_pwa import open_pwa, ChromiumNotFoundError
+from pwa_launcher.open_pwa import open_pwa
 
 __all__ = [
     "get_chromium_install",
